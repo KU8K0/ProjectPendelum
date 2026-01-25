0abcc6bf462e1a0e1aa02e1f6a73306d2eeea177
jakub <gajdosik.jakub.2009@gmail.com>
2026-01-25 15:58:27 +0100

    Added basic methods and variables in Class "Location"

--- src/Location.java
+++ src/Location.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Location {
+
+    private String id;
+    private String name;
+    private String description;
+    private List<String> neighborIds;
+
+    public Location(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.neighborIds = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void addNeighbor(String neighborId) {
+        neighborIds.add(neighborId);
+    }
+
+    public List<String> getNeighborIds() {
+        return neighborIds;
+    }
+
+    public boolean hasNeighbor(String locationId) {
+        return neighborIds.contains(locationId);
+    }
 }
