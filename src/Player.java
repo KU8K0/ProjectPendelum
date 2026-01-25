474eed027c31522842fc9c2c050668d87a8c3492
jakub <gajdosik.jakub.2009@gmail.com>
2026-01-25 16:04:31 +0100

    Added "current location of player" in class "Player"

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,12 @@
 public class Player {
+
+    private Location currentLocation;
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Location location) {
+        this.currentLocation = location;
+    }
 }
