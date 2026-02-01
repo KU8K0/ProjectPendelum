a7bbd53b75a1fc5195f8a51f2e9ee8ffb1414434
jakub <gajdosik.jakub.2009@gmail.com>
2026-02-01 15:56:05 +0100

    HelpCommand filled

--- src/Commands/HelpCommand.java
+++ src/Commands/HelpCommand.java
@@ -3,7 +3,13 @@ package Commands;
 public class HelpCommand implements Command {
     @Override
     public void execute() {
-
+        System.out.println("=== NÁPOVĚDA ===");
+        System.out.println("Dostupné příkazy:");
+        System.out.println("- jdi [kam]      (přesune tě do jiné lokace)");
+        System.out.println("- rozhledni      (popíše aktuální místo a východy)");
+        System.out.println("- batoh          (ukáže obsah inventáře)");
+        System.out.println("- mapa           (zobrazí tvou polohu vůči okolí)");
+        System.out.println("- konec          (ukončí hru)");
     }
 
     @Override
