5a642f9c5b3e6857dcee24f2105a8079f892d96a
jakub <gajdosik.jakub.2009@gmail.com>
2026-02-01 20:53:14 +0100

    Filled "TalkCommand"

--- src/Commands/TalkCommand.java
+++ src/Commands/TalkCommand.java
@@ -1,9 +1,21 @@
 package Commands;
 
+import Game.Player;
+
 public class TalkCommand implements Command {
+
+    private Player player;
+    private String target;
+
+    public TalkCommand(Player player, String target) {
+        this.player = player;
+        this.target = target;
+    }
+
     @Override
     public void execute() {
-
+        System.out.println("Mluvíš s: " + target);
+        System.out.println("Postava zatím nereaguje...");
     }
 
     @Override
