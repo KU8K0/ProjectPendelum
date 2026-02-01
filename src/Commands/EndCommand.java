296041fb113ccb5328f9dd10e409bbc9989021fa
jakub <gajdosik.jakub.2009@gmail.com>
2026-02-01 19:55:04 +0100

    Filled EndCommand

--- src/Commands/EndCommand.java
+++ src/Commands/EndCommand.java
@@ -1,13 +1,14 @@
 package Commands;
 
 public class EndCommand implements Command {
+
     @Override
     public void execute() {
-
+        System.out.println("Spojení se Silrae bylo ukončeno...");
     }
 
     @Override
     public boolean isExit() {
-        return false;
+        return true;
     }
 }
