3f46dc653be9b7ebc7c89da666267514284b2477
jakub <gajdosik.jakub.2009@gmail.com>
2026-02-01 19:11:27 +0100

    Gived inventory to player in class "Player"

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -3,6 +3,11 @@ package Game;
 public class Player {
 
     private Location currentLocation;
+    private Inventory inventory;
+
+    public Player() {
+        this.inventory = new Inventory(3); // limit 3 předměty podle designu
+    }
 
     public Location getCurrentLocation() {
         return currentLocation;
@@ -10,5 +15,9 @@ public class Player {
 
     public void setCurrentLocation(Location location) {
         this.currentLocation = location;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
     }
 }
