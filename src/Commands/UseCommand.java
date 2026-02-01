11d65bc5c4abbaede8d8f38fe54947d8f1fa15ee
jakub <gajdosik.jakub.2009@gmail.com>
2026-02-01 20:53:04 +0100

    Filled "UseCommand"

--- src/Commands/UseCommand.java
+++ src/Commands/UseCommand.java
@@ -1,9 +1,25 @@
 package Commands;
 
+import Game.Player;
+
 public class UseCommand implements Command {
+
+    private Player player;
+    private String itemName;
+
+    public UseCommand(Player player, String itemName) {
+        this.player = player;
+        this.itemName = itemName;
+    }
+
     @Override
     public void execute() {
 
+        if (player.getInventory().hasItem(itemName)) {
+            System.out.println("Použil jsi: " + itemName);
+        } else {
+            System.out.println("Tento předmět nemáš.");
+        }
     }
 
     @Override
