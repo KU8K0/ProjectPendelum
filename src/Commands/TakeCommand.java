29f7cc5994d876a58817ea2571e216b1fe46309a
jakub <gajdosik.jakub.2009@gmail.com>
2026-02-01 20:53:23 +0100

    Filled "TakeCommand"

--- src/Commands/TakeCommand.java
+++ src/Commands/TakeCommand.java
@@ -1,8 +1,26 @@
 package Commands;
 
+import Game.Player;
+import Game.Item;
+
 public class TakeCommand implements Command {
+
+    private Player player;
+    private String itemName;
+
+    public TakeCommand(Player player, String itemName) {
+        this.player = player;
+        this.itemName = itemName;
+    }
+
     @Override
-    public void execute() {}
+    public void execute() {
+
+        // Zatím testovací předmět
+        Item item = new Item("temp", itemName, "Testovací předmět");
+
+        player.getInventory().addItem(item);
+    }
 
     @Override
     public boolean isExit() {
