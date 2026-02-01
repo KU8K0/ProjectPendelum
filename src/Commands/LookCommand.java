5a314a264b47b729c8a24cebbb69829cf084c675
jakub <gajdosik.jakub.2009@gmail.com>
2026-02-01 20:45:47 +0100

    Filled "LookCommand"

--- src/Commands/LookCommand.java
+++ src/Commands/LookCommand.java
@@ -1,9 +1,30 @@
 package Commands;
 
+import Game.Player;
+import Game.Location;
+
 public class LookCommand implements Command {
+
+    private Player player;
+
+    public LookCommand(Player player) {
+        this.player = player;
+    }
+
     @Override
     public void execute() {
 
+        Location current = player.getCurrentLocation();
+
+        System.out.println("=== ROZHLED ===");
+        System.out.println(current.getName());
+        System.out.println(current.getDescription());
+        System.out.println();
+        System.out.println("Východy:");
+
+        for (String id : current.getNeighborIds()) {
+            System.out.println("- " + id);
+        }
     }
 
     @Override
