72357db29f162142374f008a8b9635cef19c822a
jakub <gajdosik.jakub.2009@gmail.com>
2026-02-01 20:54:11 +0100

    Updated "HelpCommand" and added sci-fi graphic to it

--- src/Commands/HelpCommand.java
+++ src/Commands/HelpCommand.java
@@ -1,15 +1,34 @@
 package Commands;
 
 public class HelpCommand implements Command {
+
     @Override
     public void execute() {
-        System.out.println("=== NÁPOVĚDA ===");
-        System.out.println("Dostupné příkazy:");
-        System.out.println("- jdi [kam]      (přesune tě do jiné lokace)");
-        System.out.println("- rozhledni      (popíše aktuální místo a východy)");
-        System.out.println("- batoh          (ukáže obsah inventáře)");
-        System.out.println("- mapa           (zobrazí tvou polohu vůči okolí)");
-        System.out.println("- konec          (ukončí hru)");
+
+        System.out.println("SYS_ASSIST v1.2 // ACCESS GRANTED");
+        System.out.println("====================================");
+        System.out.println();
+        System.out.println("[NAVIGACE]");
+        System.out.println("> jdi <lokace>");
+        System.out.println("> rozhledni");
+        System.out.println("> mapa");
+        System.out.println();
+        System.out.println("[INTERAKCE]");
+        System.out.println("> vezmi <předmět>");
+        System.out.println("> pouzij <předmět>");
+        System.out.println("> mluv <postava>");
+        System.out.println();
+        System.out.println("[INVENTÁŘ]");
+        System.out.println("> batoh");
+        System.out.println("> inventar");
+        System.out.println();
+        System.out.println("[SYSTÉM]");
+        System.out.println("> pomoc");
+        System.out.println("> konec");
+        System.out.println();
+        System.out.println("FOG_PROTOCOL: ACTIVE");
+        System.out.println("SURVEILLANCE_STATUS: UNKNOWN");
+        System.out.println("REALITY_STABILITY: 82%");
     }
 
     @Override
