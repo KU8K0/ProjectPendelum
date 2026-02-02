e8d661438b977e182936cf2e90a08034023d11ea
jakub <gajdosik.jakub.2009@gmail.com>
2026-02-02 10:00:00 +0100

    Updated "HelpCommand", help in text file

--- src/Commands/HelpCommand.java
+++ src/Commands/HelpCommand.java
@@ -1,34 +1,24 @@
 package Commands;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class HelpCommand implements Command {
 
     @Override
     public void execute() {
 
-        System.out.println("SYS_ASSIST v1.2 // ACCESS GRANTED");
-        System.out.println("====================================");
-        System.out.println();
-        System.out.println("[NAVIGACE]");
-        System.out.println("> jdi <lokace>");
-        System.out.println("> rozhledni");
-        System.out.println("> mapa");
-        System.out.println();
-        System.out.println("[INTERAKCE]");
-        System.out.println("> vezmi <předmět>");
-        System.out.println("> pouzij <předmět>");
-        System.out.println("> mluv <postava>");
-        System.out.println();
-        System.out.println("[INVENTÁŘ]");
-        System.out.println("> batoh");
-        System.out.println("> inventar");
-        System.out.println();
-        System.out.println("[SYSTÉM]");
-        System.out.println("> pomoc");
-        System.out.println("> konec");
-        System.out.println();
-        System.out.println("FOG_PROTOCOL: ACTIVE");
-        System.out.println("SURVEILLANCE_STATUS: UNKNOWN");
-        System.out.println("REALITY_STABILITY: 82%");
+        try (BufferedReader reader = new BufferedReader(new FileReader("Resources/help.txt"))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.out.println("HELP_ERROR: Nelze načíst nápovědu.");
+        }
     }
 
     @Override
