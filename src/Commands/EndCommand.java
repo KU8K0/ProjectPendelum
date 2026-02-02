e8b85ca36c76a425508ce2e81a824b274b372d70
jakub <gajdosik.jakub.2009@gmail.com>
2026-02-02 10:34:22 +0100

    Updated "EndCommand", outro text in text file

--- src/Commands/EndCommand.java
+++ src/Commands/EndCommand.java
@@ -1,10 +1,27 @@
 package Commands;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class EndCommand implements Command {
 
     @Override
     public void execute() {
-        System.out.println("Spojení se Silrae bylo ukončeno...");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("Resources/outro.txt"))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                Thread.sleep(100); // malý efekt postupného vypisu
+            }
+
+        } catch (IOException e) {
+            System.out.println("CONNECTION LOST...");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 
     @Override
